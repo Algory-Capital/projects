fceedb4e158b3c807105bf5ad83241dc85e82eea
Aiden Devins <p.a.devins@gmail.com>
2022-10-20 18:41:26 -0400

    Seriation

--- HRP_Model.py
+++ HRP_Model.py
@@ -1,3 +1,12 @@
+%pip install pandas 
+*pip install pandas_datareader
+*pip install numpy 
+*pip install matplotlib.pyplot 
+*pip install seaborn
+*pip install sklearn 
+*pip install datetime 
+*pip install seaborn
+
 import pandas_datareader as get_data
 import pandas as pd
 import numpy as np
