080d15a7cc81f9f3f99e27142c3c57cda24d317c
aroge23 <42726350+aroge23@users.noreply.github.com>
2023-01-21 16:39:55 -0800

    finished aum data

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const _ = require("lodash");
 const yfin = require("yahoo-finance");
 const axios = require('axios');
 const cors = require("cors");
+const e = require("express");
 
 const app = express();
 app.use(cors());
@@ -51,34 +52,31 @@ async function getData(tickers, startDate) {
   return myData;
 }
 
-const aumDates = ["2022-06-30","2022-07-01","2022-07-05","2022-07-06","2022-07-07","2022-07-08","2022-07-11","2022-07-12","2022-07-13","2022-07-14","2022-07-15","2022-07-18","2022-07-19","2022-07-20","2022-07-21","2022-07-22","2022-07-25","2022-07-26","2022-07-27","2022-07-28","2022-07-29","2022-08-01","2022-08-02","2022-08-03","2022-08-04","2022-08-05","2022-08-08","2022-08-09","2022-08-10","2022-08-11","2022-08-12","2022-08-15","2022-08-16","2022-08-17","2022-08-18","2022-08-19","2022-08-22","2022-08-23","2022-08-24","2022-08-25","2022-08-26","2022-08-29","2022-08-30","2022-08-31","2022-09-01","2022-09-02","2022-09-06","2022-09-07","2022-09-08","2022-09-09","2022-09-12","2022-09-13","2022-09-14","2022-09-15","2022-09-16","2022-09-19","2022-09-20","2022-09-21","2022-09-22","2022-09-23","2022-09-26","2022-09-27","2022-09-28","2022-09-29","2022-09-30","2022-10-03","2022-10-04","2022-10-05","2022-10-06","2022-10-07","2022-10-10","2022-10-11","2022-10-12","2022-10-13","2022-10-14","2022-10-17","2022-10-18","2022-10-19","2022-10-20","2022-10-21","2022-10-24","2022-10-25","2022-10-26","2022-10-27","2022-10-28","2022-10-31","2022-11-01","2022-11-02","2022-11-03","2022-11-04","2022-11-07","2022-11-08","2022-11-09","2022-11-10","2022-11-11","2022-11-14","2022-11-15","2022-11-16","2022-11-17", "2022-11-18"];
-
-
-const aum = [100056.78, 100059.06];
-
-for (var i = 0; i < 19; i++) {
-  aum.push(aum[aum.length - 1] * 1.000058847);
-}
-for (var i = 0; i < 23; i++) {
-  aum.push(aum[aum.length - 1] * 1.00006631);
-}
-for (var i = 0; i < 21; i++) {
-  aum.push(aum[aum.length - 1] * 1.000088122);
-}
-for (var i = 0; i < 21; i++) {
-  aum.push(aum[aum.length - 1] * 1.00027649);
-}
-const nov = [100865.52, 100313.09, 99671.21, 99804.43, 99971.61, 100115.46, 99644.79, 101062.00, 101525.29, 101416.57, 101588.03, 101460.39, 101548.17, 101727.74];
-for (var i = 0; i < nov.length; i++) {
-  aum.push(nov[i]);
-}
-
-console.log(aumDates.length);
-console.log(aum.length);
+// console.log(aumDates);
+// console.log(aum);
+// const nov = [100865.52, 100313.09, 99671.21, 99804.43, 99971.61, 100115.46, 99644.79, 101062.00, 101525.29, 101416.57, 101588.03, 101460.39, 101548.17, 101727.74, 101323];
+// for (var i = 0; i < nov.length; i++) {
+//   aum.push(nov[i]);
+// }
 
 
 
 app.get("/getData", function(req, res) {
+
+  const aumDates = ["2022-06-30","2022-07-05","2022-07-06","2022-07-07","2022-07-08","2022-07-11","2022-07-12","2022-07-13","2022-07-14","2022-07-15","2022-07-18","2022-07-19","2022-07-20","2022-07-21","2022-07-22","2022-07-25","2022-07-26","2022-07-27","2022-07-28","2022-07-29","2022-08-01","2022-08-02","2022-08-03","2022-08-04","2022-08-05","2022-08-08","2022-08-09","2022-08-10","2022-08-11","2022-08-12","2022-08-15","2022-08-16","2022-08-17","2022-08-18","2022-08-19","2022-08-22","2022-08-23","2022-08-24","2022-08-25","2022-08-26","2022-08-29","2022-08-30","2022-08-31","2022-09-01","2022-09-02","2022-09-06","2022-09-07","2022-09-08","2022-09-09","2022-09-12","2022-09-13","2022-09-14","2022-09-15","2022-09-16","2022-09-19","2022-09-20","2022-09-21","2022-09-22","2022-09-23","2022-09-26","2022-09-27","2022-09-28","2022-09-29","2022-09-30","2022-10-03","2022-10-04","2022-10-05"];
+
+
+  const aum = [100059.06];
+
+  for (var i = 0; i < 19; i++) {
+    aum.push(Number.parseFloat((aum[aum.length - 1] * 1.000058847).toFixed(2)));
+  }
+  for (var i = 0; i < 23; i++) {
+    aum.push(Number.parseFloat((aum[aum.length - 1] * 1.00006631).toFixed(2)));
+  }
+  for (var i = 0; i < 24; i++) {
+    aum.push(Number.parseFloat((aum[aum.length - 1] * 1.000088122).toFixed(2)));
+  }
 
   Equity.find({}, function(err, results) {
     if (err) {
@@ -133,16 +131,39 @@ app.get("/getData", function(req, res) {
           }
         }
 
-        var temp = js[oldestTicker];
-        if (aumDates[aumDates.length - 1] != temp.dates[temp.dates.length - 1]) {
-          aumDates.push(temp.dates[temp.dates.length - 1])
-          var lastAUM = aum[aum.length - 1];
-          for (var [ticker, value] of Object.entries(js)) {
-            lastAUM += (value.data[value.data.length - 1] - value.data[value.data.length - 2]) * value.shares;
+        // Update AUM
+        for (let i = 0; i < js[oldestTicker].data.length; i++) {
+          let addToAUM = 0;
+          for (let [ticker, value] of Object.entries(js).slice(0, -1)) {
+            let data = value.data;
+            if (i == 0 && data[i] != null) {
+              addToAUM += ((data[i] - value.entryPrice) * js[ticker].shares);
+            } else {
+              if (data[i] != null && data[i-1] != null) {
+                addToAUM += ((data[i] - data[i-1]) * js[ticker].shares);
+                // console.log(`${ticker}: ${addToAUM}`);
+              }
+            }
+            addToAUM += (aum[aum.length - 1] - addToAUM) * 0.000058847;
           }
-          lastAUM = lastAUM * 1.00007412;
-          aum.push(lastAUM);
+          // console.log(aum[aum.length - 1] + addToAUM);
+          aum.push(Number.parseFloat((aum[aum.length - 1] + addToAUM).toFixed(2)));
+          aumDates.push(js[oldestTicker].dates[i])
         }
+        console.log(aum);
+        console.log(aumDates);
+
+        // for (let ticker in js) {
+        //   if (ticker != "SPY") {
+        //     let data = js[ticker].data;
+        //     let dates = js[ticker].dates;
+        //     for (let i = 0; i < data.length; i++) {
+        //       if (data[i] != null) {
+        //         console.log(data[i]);
+        //       }
+        //     }
+        //   }
+        // }
 
         js["AUM"] = {
           dates: aumDates,
@@ -151,6 +172,7 @@ app.get("/getData", function(req, res) {
 
         res.send(js);
       }).catch((err) => {
+        res.send(err);
         console.log(err);
       });
     }
