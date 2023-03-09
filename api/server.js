2c9d7cf5b89a93011ebbe03acce957c0d9e4a9e1
aroge23 <42726350+aroge23@users.noreply.github.com>
2023-03-09 17:57:23 +0100

    update dashboard

--- api/server.js
+++ api/server.js
@@ -52,32 +52,9 @@ async function getData(tickers, startDate) {
   return myData;
 }
 
-// console.log(aumDates);
-// console.log(aum);
-// const nov = [100865.52, 100313.09, 99671.21, 99804.43, 99971.61, 100115.46, 99644.79, 101062.00, 101525.29, 101416.57, 101588.03, 101460.39, 101548.17, 101727.74, 101323];
-// for (var i = 0; i < nov.length; i++) {
-//   aum.push(nov[i]);
-// }
-
 
 
 app.get("/getData", function(req, res) {
-
-  const aumDates = ["2022-06-30","2022-07-05","2022-07-06","2022-07-07","2022-07-08","2022-07-11","2022-07-12","2022-07-13","2022-07-14","2022-07-15","2022-07-18","2022-07-19","2022-07-20","2022-07-21","2022-07-22","2022-07-25","2022-07-26","2022-07-27","2022-07-28","2022-07-29","2022-08-01","2022-08-02","2022-08-03","2022-08-04","2022-08-05","2022-08-08","2022-08-09","2022-08-10","2022-08-11","2022-08-12","2022-08-15","2022-08-16","2022-08-17","2022-08-18","2022-08-19","2022-08-22","2022-08-23","2022-08-24","2022-08-25","2022-08-26","2022-08-29","2022-08-30","2022-08-31","2022-09-01","2022-09-02","2022-09-06","2022-09-07","2022-09-08","2022-09-09","2022-09-12","2022-09-13","2022-09-14","2022-09-15","2022-09-16","2022-09-19","2022-09-20","2022-09-21","2022-09-22","2022-09-23","2022-09-26","2022-09-27","2022-09-28","2022-09-29","2022-09-30","2022-10-03","2022-10-04","2022-10-05"];
-
-
-  const aum = [100059.06];
-
-  for (var i = 0; i < 19; i++) {
-    aum.push(Number.parseFloat((aum[aum.length - 1] * 1.000058847).toFixed(2)));
-  }
-  for (var i = 0; i < 23; i++) {
-    aum.push(Number.parseFloat((aum[aum.length - 1] * 1.00006631).toFixed(2)));
-  }
-  for (var i = 0; i < 24; i++) {
-    aum.push(Number.parseFloat((aum[aum.length - 1] * 1.000088122).toFixed(2)));
-  }
-
   Equity.find({}, function(err, results) {
     if (err) {
       console.log(err);
@@ -86,8 +63,12 @@ app.get("/getData", function(req, res) {
       var js = {};
       var tickers = [];
       var startDates = [];
+      const aum = [100536.24];
+      var aumDates = [];
       var shares = [];
       var entryPrice = [];
+      var aumPercentage = [];
+      var spyPercentage = [0.00];
       results.forEach(function(result) {
         tickers.push(result.ticker);
         startDates.push(result.startDate);
@@ -141,33 +122,31 @@ app.get("/getData", function(req, res) {
             } else {
               if (data[i] != null && data[i-1] != null) {
                 addToAUM += ((data[i] - data[i-1]) * js[ticker].shares);
-                // console.log(`${ticker}: ${addToAUM}`);
               }
             }
-            addToAUM += (aum[aum.length - 1] - addToAUM) * 0.000058847;
+            addToAUM += (aum[aum.length - 1] - addToAUM) * 0.000038847;
           }
-          // console.log(aum[aum.length - 1] + addToAUM);
           aum.push(Number.parseFloat((aum[aum.length - 1] + addToAUM).toFixed(2)));
-          aumDates.push(js[oldestTicker].dates[i])
+          aumDates.push(js[oldestTicker].dates[i]);
+          if (i == 0) {aum.shift();}
         }
-        console.log(aum);
-        console.log(aumDates);
 
-        // for (let ticker in js) {
-        //   if (ticker != "SPY") {
-        //     let data = js[ticker].data;
-        //     let dates = js[ticker].dates;
-        //     for (let i = 0; i < data.length; i++) {
-        //       if (data[i] != null) {
-        //         console.log(data[i]);
-        //       }
-        //     }
-        //   }
-        // }
+        // AUM percentage
+        for (let i = 0; i < aum.length; i++) {
+          aumPercentage.push(Number(((aum[i] - 100536.24) / 100536.24 * 100).toFixed(3)));
+        }
+
+        // SPY percentage
+        for (let i = 1; i < js["SPY"].data.length; i++) {
+          spyPercentage.push(Number(((js["SPY"].data[i] - js["SPY"].data[0]) / js["SPY"].data[0] * 100).toFixed(3)));
+        }
+
+        js["SPY"].spyPercentage = spyPercentage;
 
         js["AUM"] = {
           dates: aumDates,
-          aum: aum
+          aum: aum,
+          aumPercentage: aumPercentage
         }
 
         res.send(js);
