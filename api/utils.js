cfdcc82f573579a898950937893bac90cfa2d11c
Fobertree <aliu266@emory.edu>
2025-01-14 18:38:27 -0500

    im an idiot - es6 vs commonjns push

--- api/utils.js
+++ api/utils.js
@@ -1,9 +1,13 @@
 // get startDates (migrate part of /getData code)
 
-export function isEmpty(obj) {
-    for(var key in obj) {
-      if(obj.hasOwnProperty(key))
-        return false;
-    }
-    return true;
+function isEmpty(obj) {
+  for(var key in obj) {
+    if(obj.hasOwnProperty(key))
+      return false;
   }
+  return true;
+}
+
+module.exports = {
+  isEmpty
+}
