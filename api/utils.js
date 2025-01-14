0201925ff6acbb792e33e7babc1a171272f625ad
Fobertree <aliu266@emory.edu>
2025-01-14 18:35:05 -0500

    add utils

---
+++ api/utils.js
@@ -0,0 +1,9 @@
+// get startDates (migrate part of /getData code)
+
+export function isEmpty(obj) {
+    for(var key in obj) {
+      if(obj.hasOwnProperty(key))
+        return false;
+    }
+    return true;
+  }
